fd315ff19151253cd01e9c61df97efdddeb4f22c
Nikolai V. Fredriksen <cooprix1@gmail.com>
2026-05-22 14:46:24 +0200

    Fikset scroll-margin-top for sidebar på begge devices

--- src/components/Nominations.jsx
+++ src/components/Nominations.jsx
@@ -108,7 +108,6 @@ const Nominations = ({
           <div
             key={cat.category}
             id={cat.category.replace(/\s+/g, "-").toLowerCase()}
-            style={{ scrollMarginTop: "100px" }}
           >
             <h2 className={`${index === 0 ? "mt-15" : "mt-20"} mb-2`}>
               {cat.category}
